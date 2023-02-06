f9a15de6c402150d554266fe70e3459335f96447
TM_ <35858330+macthieuminh@users.noreply.github.com>
2023-02-06 21:28:48 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import { shoeList } from "./data";
+import {
+    PlusOutlined,
+    LoadingOutlined,
+    LeftOutlined,
+    RightOutlined,
+} from "@ant-design/icons";
+import { Space, Switch } from "antd";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [index, setIndex] = useState(0);
+    const [isActive, setIsActive] = useState(true);
+    let shoe = shoeList[index];
+    const handleChange = () => {
+        setIsActive((current) => !current);
+    };
+    function handleNext() {
+        if (index <= 3) {
+            setIndex(index + 1);
+        } else {
+            setIndex(0);
+        }
+    }
+    function handlePrev() {
+        if (index <= 3) {
+            setIndex(index - 1);
+        } else {
+            setIndex(0);
+        }
+    }
+    return (
+        <div className="App">
+            <div
+                className="wrap"
+                style={{
+                    height: isActive ? "300px" : "20px",
+                    width: isActive ? "320px" : "120px",
+                }}>
+                <div className="nav-bar">
+                    <div className="card__heart">
+                        <Space>
+                            <Switch defaultChecked onChange={handleChange} />
+                            <LoadingOutlined
+                                style={{
+                                    display: isActive ? "none" : "block",
+                                    color: "#fff",
+                                    fontSize: "30px",
+                                    marginLeft: "48px",
+                                }}
+                                spin
+                            />
+                        </Space>
+                    </div>
+                    <div
+                        className="card__cart"
+                        style={{ display: isActive ? "block" : "none" }}>
+                        <Space>
+                            <PlusOutlined
+                                style={{ fontSize: "30px", color: "#fff" }}
+                            />
+                        </Space>
+                    </div>
+                </div>
+                <div
+                    className="card"
+                    style={{
+                        display: isActive ? "block" : "none",
+                    }}>
+                    <div className="card__img">
+                        <img src={shoe.url} alt={shoe.alt} />
+                    </div>
+                    <div className="card__title">{shoe.name}</div>
+                    <div className="card__price">${shoe.price}</div>
+                    <div className="rest">
+                        <div className="card__size">
+                            <h3 className="info">Size: </h3>
+                            <span>44</span>
+                            <span>44.5</span>
+                            <span>45</span>
+                            <span>45.5</span>
+                            <span>46</span>
+                        </div>
+                        <div className="card__color">
+                            <h3 className="info">Color: </h3>
+                            <span className="card__color--white"></span>
+                            <span className="card__color--black"></span>
+                        </div>
+                        <div className="card__actions">
+                            <Space
+                                style={{
+                                    display: "flex",
+                                    justifyContent: "space-around",
+                                    alignItems: "center",
+                                }}>
+                                <LeftOutlined
+                                    style={{ fontSize: "32px" }}
+                                    onClick={handlePrev}
+                                />
+                                <RightOutlined
+                                    style={{ fontSize: "32px" }}
+                                    onClick={handleNext}
+                                />
+                            </Space>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const shoeList = [
+    {
+        id: 1,
+        name: "Nike Zoom Fly 5 Premium",
+        color: "White/Racer",
+        desc: "Bridge the gap between your weekend training run and race day in this durable design. Wear it for your favourite race and in the days after your conquest. It offers comfort and reliability but with a propulsive sensation that helps you feel fast and fresh. That kind of versatility is uncommon in the running arena. But who said you cant have it all? Its a radiant, empowering twist on a running essential, inspired by our human pacers who inspire through connection.",
+        url: "https://static.nike.com/a/images/t_default/b50d0ce7-f89a-422a-add5-442b72711cd7/zoom-fly-5-road-running-shoes-tMJ7vL.png",
+        alt: "Mens Road Running Shoes, Nike Zoom Fly 5 Premium White",
+        price: 299,
+    },
+    {
+        id: 2,
+        name: "Nike Invincible 3",
+        color: "White",
+        desc: "With maximum cushioning to support every mile, the Invincible 3 gives you our highest level of comfort underfoot to help you stay on your feet today, tomorrow and beyond. Designed to help keep you on the run, its super springy and bouncy so that you can propel down your preferred path and come back for your next run feeling ready and reinvigorated.",
+        url: "https://static.nike.com/a/images/t_default/b03907b4-a52f-46a8-b195-2e79ded93212/invincible-3-road-running-shoes-Wwmmlp.png",
+        alt: "Mens Road Running Shoes, Nike Invincible 3",
+        price: 399,
+    },
+    {
+        id: 3,
+        name: "Nike Alphafly 2",
+        color: "Bright Crimson",
+        desc: "Once you take a few strides in the Nike Alphafly 2, you'll never look at your favourite pair of old racing shoes in the same way. These rocket ships are made to help shave precious time off your personal records without surrendering the foundation you need to go the full distance. A thick, lightweight support system brings comfort and speed together so you can enjoy our greatest energy return while you chase your personal bests.",
+        url: "https://static.nike.com/a/images/t_default/589e8719-84e3-43ef-84cf-c22cb5c1513a/alphafly-2-road-racing-shoes-MRSlSl.png",
+        alt: "Mens Road Running Shoes, Nike Alphafly 2",
+        price: 499,
+    },
+];
